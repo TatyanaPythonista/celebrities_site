e84ff458670b6c57f86555a6f2f4ee4e355541a3
Tatyana Egorenkova <tatyana.baeva.1990@gmail.com>
2021-03-01 23:27:45 +0700

    eight lesson

--- girlcelebritiessite/women/urls.py
+++ girlcelebritiessite/women/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('categories/<int:category_id>/', categories),
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
+    path('add_page/', add_page, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>', show_post, name='post')
 ]
--- girlcelebritiessite/women/views.py
+++ girlcelebritiessite/women/views.py
@@ -1,21 +1,39 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from .models import *
+
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связь', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'}]
 
 
 def index(request):
-    return HttpResponse('Страница приложения Women')
+    posts = Women.objects.all()
+    context = {'posts': posts,
+               'menu': menu,
+               'title': 'Главная страница'}
+    return render(request, 'women/index.html', context=context)
 
 
-def categories(request, category_id):
-    if request.GET:
-        print(request.GET)
-    return HttpResponse(f'<h1>Статьи по категориям.</h1><p>{category_id}</p>')
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def archive(request, year):
-    if int(year) > 2020:
-        return redirect('home', permanent=True)
-    return HttpResponse(f'<h1>Архив по годам</h1><p>{year}</p>')
+def add_page(request):
+    return HttpResponse('Добавление статьи')
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь.')
+
+
+def login(request):
+    return HttpResponse('Страница входа.')
 
 
 def pageNotFound(request, exception):
