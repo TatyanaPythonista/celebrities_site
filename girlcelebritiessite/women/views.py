a5fd9e244341998204a47b22a08944b7f77e3333
Tatyana Egorenkova <tatyana.baeva.1990@gmail.com>
2021-04-03 22:31:51 +0700

    user sign in

--- girlcelebritiessite/women/views.py
+++ girlcelebritiessite/women/views.py
@@ -1,11 +1,13 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-from .forms import RegistrationUserForm, AddPostForm
+from .forms import RegistrationUserForm, AddPostForm, LoginUserForm
 from .models import *
 from .utils import DataMixin, menu
 
@@ -59,10 +61,6 @@ def contact(request):
     return HttpResponse('Обратная связь.')
 
 
-def login(request):
-    return HttpResponse('Страница входа.')
-
-
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
@@ -93,5 +91,25 @@ class RegistrationUser(DataMixin, CreateView):
         context_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(context_def.items()))
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
 
 
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(context_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
